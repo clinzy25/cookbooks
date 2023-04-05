9acfa57fb6719b59a618691b479a509c708a299c
clinzy25 <conner.linzy@area4labs.com>
2023-04-05 11:41:58 -0700

    schema first pass

--- server/src/db/migrations/20230405171601_cookbooks_migration.ts
+++ server/src/db/migrations/20230405171601_cookbooks_migration.ts
@@ -1,10 +1,98 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id').primary();
+      table.string('username', 100).notNullable();
+      table.string('email', 100).notNullable();
+      table.string('password', 250).notNullable();
+      table.timestamps();
+    })
+    .createTable('cookbook_members', function (table) {
+      table.increments('id').primary();
+      table.foreign('creator_user_id').references('users.id');
+      table.foreign('member_user_id').references('users.id');
+      table.foreign('cookbook_id').references('cookbooks.id');
+      table.timestamps();
+    })
+    .createTable('invites', function (table) {
+      table.increments('id').primary();
+      table.foreign('sender_user_id').references('users.id');
+      table.foreign('recipient_user_id').references('users.id');
+      table.foreign('cookbook_id').references('cookbooks.id');
+      table.integer('accepted').notNullable();
+      table.timestamps();
+    })
+    .createTable('cookbooks', function (table) {
+      table.increments('id').primary();
+      table.foreign('creator_user_id').references('users.id');
+      table.string('cookbook_name', 100);
+      table.timestamps();
+    })
+    .createTable('recipes', function (table) {
+      table.increments('id').primary();
+      table.foreign('cookbook_id').references('cookbooks.id');
+      table.foreign('user_id').references('users.id');
+      table.string('recipe_name', 100).notNullable();
+      table.string('image', 250);
+      table.text('description', 'text');
+      table.integer('servings');
+      table.string('prep_time', 100);
+      table.string('cook_time', 100);
+      table.string('author_name', 100);
+      table.integer('is_private').notNullable().defaultTo(0);
+      table.timestamps();
+    })
+    .createTable('ingredients', function (table) {
+      table.increments('id').primary();
+      table.foreign('ingredient_type_id').references('ingredient_types.id');
+      table.foreign('recipe_id').references('recipes.id');
+      table.string('unit', 50).notNullable();
+      table.integer('amount').notNullable();
+      table.timestamps();
+    })
+    .createTable('ingredient_types', function (table) {
+      table.increments('id').primary();
+      table.string('ingredient_name', 100).notNullable();
+      table.timestamps();
+    })
+    .createTable('instructions', function (table) {
+      table.increments('id').primary();
+      table.foreign('recipe_id').references('recipes.id');
+      table.text('instruction_body', 'text').notNullable();
+      table.timestamps();
+    })
+    .createTable('notes', function (table) {
+      table.increments('id').primary();
+      table.foreign('recipe_id').references('recipes.id');
+      table.text('note_body', 'text').notNullable();
+      table.timestamps();
+    })
+    .createTable('tags', function (table) {
+      table.increments('id').primary();
+      table.foreign('recipe_id').references('recipes.id');
+      table.foreign('tag_type_id').references('tag_types.id');
+      table.timestamps();
+    })
+    .createTable('tag_types', function (table) {
+      table.increments('id').primary();
+      table.string('tag_name', 50).notNullable();
+      table.timestamps();
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema
+    .dropTable('users')
+    .dropTable('cookbook_members')
+    .dropTable('invites')
+    .dropTable('cookbooks')
+    .dropTable('recipes')
+    .dropTable('ingredients')
+    .dropTable('ingredient_types')
+    .dropTable('instructions')
+    .dropTable('notes')
+    .dropTable('tags')
+    .dropTable('tag_types');
 }
-
