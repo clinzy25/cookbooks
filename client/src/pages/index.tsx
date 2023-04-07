f0a4b2ba3fc266a9de7d5e003b0257711682c243
clinzy25 <conner.linzy@area4labs.com>
2023-04-07 14:32:28 -0700

    landing page v1

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,41 @@
-const LandingPage = () => <p>LandingPage</p>
+import styled from 'styled-components'
+
+const LandingPage = () => (
+  <Styles>
+    <header>
+      <h1>Cookbooks</h1>
+      <p>Create a shared cookbook.</p>
+    </header>
+    <div id='btn-ctr'>
+      <button>Login</button>
+      <button>Signup</button>
+    </div>
+  </Styles>
+)
+
+const Styles = styled.main`
+  header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 300px;
+    font-family: 'Nunito Sans', sans-serif;
+    h1,
+    p {
+      margin-top: auto;
+    }
+  }
+  #btn-ctr {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    button {
+      padding: 10px 20px;
+      margin: 10px;
+    }
+  }
+`
 
 export default LandingPage
