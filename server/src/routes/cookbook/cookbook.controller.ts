f1b0a4c3c0f84d7a07b58d56a451c292da8ba236
clinzy25 <conner.linzy@area4labs.com>
2023-04-14 15:18:47 -0700

    refactor controllers

--- server/src/routes/cookbook/cookbook.controller.ts
+++ server/src/routes/cookbook/cookbook.controller.ts
@@ -4,22 +4,22 @@ import { createCookbook, getCookbooks } from '../../model/cookbook.model'
 export async function httpGetCookbooks(req: Request, res: Response) {
   const user_guid = req.query.user_guid?.toString()
 
-  if (user_guid) {
-    const cookbooks = await getCookbooks(user_guid)
-    return res.status(200).json(cookbooks)
+  if (!user_guid) {
+    return res.status(404).json('Missing required params')
   }
-  return res.status(404).json('Missing required params')
+  const cookbooks = await getCookbooks(user_guid)
+  return res.status(200).json(cookbooks)
 }
 
 export async function httpCreateCookbook(req: Request, res: Response) {
   const cookbook = req.body.newCookbook
 
-  if (cookbook) {
-    const result = await createCookbook(cookbook)
-    if (result?.rows?.[0]?.cookbook_name === cookbook.cookbook_name) {
-      return res.status(201).json('Cookbook creation successful')
-    }
+  if (!cookbook) {
+    return res.status(404).json('Missing body of the request')
+  }
+  const result = await createCookbook(cookbook)
+  if (!result?.rows?.[0]?.cookbook_name === cookbook.cookbook_name) {
     return res.status(500).json('Cookbook creation failed')
   }
-  return res.status(404).json('Missing body of the request')
+  return res.status(201).json('Cookbook creation successful')
 }
--- server/src/routes/recipe/recipe.controller.ts
+++ server/src/routes/recipe/recipe.controller.ts
@@ -4,21 +4,21 @@ import { getCookbookRecipes, getRecipe } from '../../model/recipe.model'
 export async function httpGetCookbookRecipes(req: Request, res: Response) {
   const cookbook = req.query.cookbook?.toString()
 
-  if (cookbook) {
-    const recipes = await getCookbookRecipes(cookbook)
-    return res.status(200).json(recipes)
+  if (!cookbook) {
+    return res.status(404).json('Missing required params')
   }
-  return res.status(404).json('Missing required params')
+  const recipes = await getCookbookRecipes(cookbook)
+  return res.status(200).json(recipes)
 }
 
 export async function httpGetRecipe(req: Request, res: Response) {
   const recipe_guid = req.query.recipe_guid?.toString()
 
-  if (recipe_guid) {
-    const recipe = await getRecipe(recipe_guid)
-    return res.status(200).json(recipe)
+  if (!recipe_guid) {
+    return res.status(404).json('Missing required params')
   }
-  return res.status(404).json('Missing required params')
+  const recipe = await getRecipe(recipe_guid)
+  return res.status(200).json(recipe)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
