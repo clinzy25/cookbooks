508376319252bfa93ddd5484e0b22ddc07b1732c
clinzy25 <conner.linzy@area4labs.com>
2023-05-02 11:09:29 -0700

    cleanup unused

--- client/src/utils/utils.errors.client.ts
+++
@@ -1,5 +0,0 @@
-export const FIELD_REQUIRED = 'errors/invalid-url'
-export const FIELD_REQUIRED_MSG = 'This field is required'
-
-export const clientErrorMessages = new Map()
-clientErrorMessages.set(FIELD_REQUIRED, FIELD_REQUIRED_MSG)
--- client/src/utils/utils.hooks.ts
+++ client/src/utils/utils.hooks.ts
@@ -15,22 +15,3 @@ export const useOutsideAlerter = (ref: RefObject<HTMLElement>, task: () => void)
     }
   }, [ref, task])
 }
-
-export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  })
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      })
-    }
-    window.addEventListener('resize', handleResize)
-    handleResize()
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-  return windowSize
-}
