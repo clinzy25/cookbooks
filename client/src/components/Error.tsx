2481ef87cf5cb55a78c238379ca63b6d6e64af72
clinzy25 <conner.linzy@area4labs.com>
2023-05-12 09:56:21 -0700

    error component

---
+++ client/src/components/Error.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+type Props = {
+  fillSpace?: boolean
+  fontSize?: number
+}
+
+const Error: FC<Props> = ({ fillSpace, fontSize = 1.5 }) => {
+  return (
+    <Style
+      className={fillSpace ? 'page-wrapper' : ''}
+      fillSpace={fillSpace}
+      fontSize={fontSize}>
+      Something went wrong :(
+    </Style>
+  )
+}
+
+const Style = styled.div<Props>`
+  ${props =>
+    props.fillSpace &&
+    `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%
+  `};
+  font-size: ${props => props.fontSize}rem;
+  font-family: ${({ theme }) => theme.headerFont};
+`
+
+export default Error
