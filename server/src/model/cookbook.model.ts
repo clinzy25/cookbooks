e7fd4e8a2b8a92154884b3a9b507ae0e426e4e73
clinzy25 <conner.linzy@area4labs.com>
2023-05-18 12:45:43 -0700

    sql syntax

--- server/src/model/cookbook.model.ts
+++ server/src/model/cookbook.model.ts
@@ -11,7 +11,7 @@ export async function dbGetCookbooks(user_guid: string) {
           c.cookbook_name,
           c.created_at,
           c.updated_at,
-          COUNT(DISTINCT r.id) as recipe_count,
+          COUNT(DISTINCT r.id) AS recipe_count,
           (ARRAY_AGG(DISTINCT JSONB_BUILD_OBJECT(
             'image', r.image, 
             'base64_image', r.base64_image
@@ -24,12 +24,12 @@ export async function dbGetCookbooks(user_guid: string) {
         FROM cookbooks c
         JOIN users u ON u.id = c.creator_user_id
         LEFT JOIN cookbook_members cm ON cm.cookbook_id = c.id
-        LEFT JOIN recipes r on r.cookbook_id = c.id
+        LEFT JOIN recipes r ON r.cookbook_id = c.id
         LEFT JOIN LATERAL (
-          SELECT u.guid, u.email, u.username, c.id as cookbook_id
+          SELECT u.guid, u.email, u.username, c.id AS cookbook_id
           FROM cookbook_members cm
-          JOIN users u on u.id = cm.member_user_id
-          JOIN cookbooks c on c.id = cm.cookbook_id
+          JOIN users u ON u.id = cm.member_user_id
+          JOIN cookbooks c ON c.id = cm.cookbook_id
           WHERE c.id = cm.cookbook_id
           LIMIT 10
         ) member_sub ON member_sub.cookbook_id = c.id
