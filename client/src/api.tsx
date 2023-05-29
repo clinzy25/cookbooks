694db7b8fcb6062371e4c418168c1bfce8734be9
clinzy25 <conner.linzy@area4labs.com>
2023-05-29 11:45:53 -0700

    production tweaks

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api =
   process.env.NODE_ENV === 'development'
     ? process.env.NEXT_PUBLIC_DEV_API
-    : process.env.NEXT_PUBLIC_DEV_API
+    : process.env.NEXT_PUBLIC_PROD_API
 
 export const fetcher = async (url: string) => await axios.get(url).then(res => res.data)
